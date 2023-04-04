f9e9a1e6752c222bfe1d35587829e01806219e3c
Vahid <5300terminalads@gmail.com>
2023-04-04 17:41:02 +0330

    fix bottom menu design

--- layout/bottomMenu/container.vue
+++ layout/bottomMenu/container.vue
@@ -1,22 +1,24 @@
 <template>
   <v-row no-gutters class="bottom-btn-group-row">
 
-    <v-btn-toggle group v-model="toggle_exclusive" class="button-btn-group">
+    <v-btn-toggle group v-model="toggle_exclusive" class="button-btn-group" mandatory>
       <div v-for="(item, i) in btnGroup" :key="i" class="btn-toggle-holder">
-        <v-btn v-if="item.link" :href="typeof item.link === 'string' ? item.link : ''" @click="handleCLick(item)"
-               class="rounded-pill" depressed height="32" min-width="32" width="32" text :aria-label="item.title"
-               color="primary lighten-1">
+        <v-btn v-if="item.link" :href="typeof item.link === 'string' ? item.link : ''" @click="handleCLick(item, i)"
+               class="rounded-pill" color="grey" depressed height="32" min-width="32" width="32" text
+               :aria-label="item.title">
           <v-icon>mdi-{{ item.icon }}</v-icon>
         </v-btn>
 
         <component v-else :is="item.component" :icon="item.icon">
-          <v-btn class="rounded-pill" depressed height="32" min-width="32" width="32" text :aria-label="item.title"
-                 color="primary lighten-1">
+          <v-btn class="rounded-pill" color="grey" depressed height="32" min-width="32" width="32" text
+                 :aria-label="item.title" @click="index = i">
             <v-icon>mdi-{{ item.icon }}</v-icon>
           </v-btn>
         </component>
 
-        <span class="pt-1 primary--text">{{ item.title }}</span>
+        <span class="pt-1 grey--text menu-subtitle" :class="item.index === index ? 'd-none' : ''">{{
+            item.title
+          }}</span>
       </div>
     </v-btn-toggle>
 
@@ -34,23 +36,25 @@ export default {
   components: {MoreButton, NotifyMenu},
 
   data: () => ({
-    toggle_exclusive: null,
+    toggle_exclusive: 0,
     btnGroup: [],
-    width: 0
+    width: 0,
+    index: 0
   }),
 
   mounted() {
     this.btnGroup = [
-      {link: 'https://core.terminalads.com', icon: 'home', title: 'خانه'},
-      {link: this.handleMenu, icon: 'menu', title: 'منو'},
-      {link: null, icon: 'bell', title: 'اعلانات', component: 'NotifyMenu'},
-      {link: this.handleHelp, icon: 'help', title: 'راهنما'},
-      {link: null, icon: 'dots-vertical', title: 'بیشتر', component: 'MoreButton'},
+      {link: 'https://core.terminalads.com', icon: 'home', title: 'خانه', index: 0},
+      {link: this.handleMenu, icon: 'menu-open', title: 'منو', index: 1},
+      {link: null, icon: 'bell', title: 'اعلانات', component: 'NotifyMenu', index: 2},
+      {link: this.handleHelp, icon: 'help', title: 'راهنما', index: 3},
+      {link: null, icon: 'dots-vertical', title: 'بیشتر', component: 'MoreButton', index: 4},
     ]
   },
 
   methods: {
-    handleCLick(item) {
+    handleCLick(item, i) {
+      this.index = i
       if (typeof item.link === 'function') {
         let btn = document.createElement('button')
         btn.addEventListener('click', item.link)
@@ -76,19 +80,17 @@ export default {
 .bottom-btn-group-row {
   background-color: white;
   overflow: hidden;
-  height: 55px;
+  height: 62px;
   position: fixed;
-  bottom: 6px;
+  bottom: 0;
   z-index: 500;
-  left: 50%;
-  width: calc(100% - 12px);
-  border-radius: 12px;
-  transform: translateX(-50%);
+  left: 0;
+  width: 100%;
+  border-radius: 12px 12px 0 0;
   display: flex;
   align-items: flex-end;
   justify-content: center;
-  box-shadow: 0 -5px 15px 5px rgba(0, 0, 0, .25),
-  0 -5px 10px 0 rgba(0, 0, 0, .05);
+  box-shadow: 0 -5px 15px 5px rgba(0, 0, 0, .25), 0 -5px 10px 0 rgba(0, 0, 0, .05);
 }
 
 .button-btn-group {
@@ -97,6 +99,7 @@ export default {
   display: flex;
   justify-content: space-around;
   padding-bottom: 3px;
+  align-self: center;
 }
 
 .btn-toggle-holder {
@@ -113,10 +116,11 @@ export default {
 /*    display: none;*/
 /*}*/
 
-.btn-toggle-holder.v-item--active button {
-  /*transition: all .2s ease-in;*/
-  /*transform: translateY(8px);*/
-  color: var(--v-primary-base);
+.btn-toggle-holder :deep(.v-item--active) {
+  transition: all .2s ease-in;
+  transform: translateY(8px);
+  color: var(--v-primary-base) !important;
+  caret-color: var(--v-primary-base) !important;
 }
 
 .btn-toggle-holder span {
