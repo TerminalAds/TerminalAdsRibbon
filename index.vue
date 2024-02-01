6289c98e78deefc52eba39e5c98acf5e3e499d05
erfan <erfan.rahbari123@gmail.com>
2024-02-01 13:34:49 +0330

    add:goftino

--- index.vue
+++ index.vue
@@ -67,6 +67,21 @@ import Tutorials from "./pages/tutorials";
 import BottomMenuContainer from "./layout/bottomMenu/container";
 import RefreshPage from "./layout/header/refreshPage";
 
+
+!function () {
+  var i = "7WvGxT", a = window, d = document;
+
+  function g() {
+    var g = d.createElement("script"), s = "https://www.goftino.com/widget/" + i,
+        l = localStorage.getItem("goftino_" + i);
+    g.async = !0, g.src = l ? s + "?o=" + l : s;
+    d.getElementsByTagName("head")[0].appendChild(g);
+  }
+
+  "complete" === d.readyState ? g() : a.attachEvent ? a.attachEvent("onload", g) : a.addEventListener("load", g, !1);
+}();
+
+
 export default {
   name: "index",
 
