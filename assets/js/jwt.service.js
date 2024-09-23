b53f480596fd9db6a662e1ad49474bcee2003c20
vahid <5300terminalads@gmail.com>
2024-09-23 15:08:12 +0330

    change: userPicker field

--- assets/js/jwt.service.js
+++ assets/js/jwt.service.js
@@ -1,23 +1,25 @@
 const ID_TOKEN_KEY = "id_token";
 
 export const getToken = () => {
-    return window.localStorage.getItem(ID_TOKEN_KEY);
+  return window.localStorage.getItem(ID_TOKEN_KEY);
 };
 
 export const saveToken = token => {
-    window.localStorage.setItem(ID_TOKEN_KEY, token);
+  window.localStorage.setItem(ID_TOKEN_KEY, token);
 };
 
 export const destroyToken = (origin, hash) => {
-    window.localStorage.removeItem(ID_TOKEN_KEY);
-    localStorage.removeItem('reloaded');
-    window.localStorage.removeItem('userInfo');
+  window.localStorage.removeItem(ID_TOKEN_KEY);
+  localStorage.removeItem('reloaded');
+  window.localStorage.removeItem('userInfo');
 
-    (origin && hash)
-        ? window.location.replace(`https://core.terminalads.com/#/login?call_back_origin=${origin}&call_back_hash=${hash}`)
-        : window.location.origin === 'https://core.terminalads.com'
-            ? window.location.reload()
-            : window.location.replace(`https://core.terminalads.com/#/login`)
+  console.log('jwt destroyToken: ', origin, hash)
+
+  (origin && hash)
+      ? window.location.replace(`https://core.terminalads.com/#/login?call_back_origin=${origin}&call_back_hash=${hash}`)
+      : window.location.origin === 'https://core.terminalads.com'
+          ? window.location.reload()
+          : window.location.replace(`https://core.terminalads.com/#/login`)
 };
 
 export default {getToken, saveToken, destroyToken};
