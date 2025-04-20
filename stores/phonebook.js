8e1b78ae86cc62171ef92855e80e6fb27106c435
Vahid <vahidesnaashari79@gmail.com>
2025-04-21 01:06:10 +0330

    fix: phonebook store

--- stores/phonebook.js
+++ stores/phonebook.js
@@ -3,11 +3,12 @@ export default {
     phonebooks_list: [],
     selected_phonebook_index: 0,
     selectedPhonebook: null,
+    imported_phonebooks: {}
   },
   getters: {
     active_phonebook: state => state.phonebooks_list[state.selected_phonebook_index],
     isSharedPhonebook: (state, getters) => getters.active_phonebook?.hasOwnProperty('parent_id'),
-    phonebookPermissions: (state, getters) => getters.isSharedPhonebook ? getters.active_phonebook.setting : true
+    phonebookPermissions: (state, getters) => getters.isSharedPhonebook ? getters.active_phonebook.setting : true,
   },
   actions: {
     setPhonebooksList({commit}, list) {
@@ -19,6 +20,9 @@ export default {
     setSelectedPhonebook({commit}, tag) {
       commit('set_selected_phonebook', tag)
     },
+    updateImportedPhonebooks({commit}, input) {
+      commit('set_imported_phonebooks', input)
+    }
   },
   mutations: {
     set_phonebooks_list(state, list) {
@@ -30,6 +34,11 @@ export default {
     set_selected_phonebook(state, tag) {
       state.selectedPhonebook = tag
     },
+    set_imported_phonebooks(state, input) {
+      console.log('set_imported_phonebooks', input)
+
+      state.imported_phonebooks = input
+    }
   },
   namespaced: true,
 };
