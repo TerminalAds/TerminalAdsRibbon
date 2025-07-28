d568701b3ca29da295caf22805b9e5668685c7df
homa <5700terminalads@gmail.com>
2025-07-28 12:01:15 +0330

    add 63

--- components/contacts/SuggestBox/emoji.vue
+++ components/contacts/SuggestBox/emoji.vue
@@ -1,16 +1,30 @@
 <template>
   <v-card flat>
     <v-card-text>
-      <div class="bg-white" style="max-height: 18rem;overflow: auto;direction: ltr;text-align: left">
+      <div
+        class="bg-white"
+        style="
+          max-height: 18rem;
+          overflow: auto;
+          direction: ltr;
+          text-align: left;
+        "
+      >
         <div v-for="group in emojiGroup" :key="group" class="row mx-auto">
           <div class="col-12">
             <h5>{{ group }}</h5>
 
-            <hr>
+            <hr />
           </div>
+          <!--  -->
 
-          <div v-for="emoji in emojis.filter(emoji => emoji.group === group)" :data-slug="emoji.slug" class="col fa-2x"
-               style="color: black;cursor: pointer" @click="selectEmoji(emoji.icon)">
+          <div
+            v-for="emoji in emojis.filter((emoji) => emoji.group === group)"
+            :data-slug="emoji.slug"
+            class="col fa-2x"
+            style="color: black; cursor: pointer"
+            @click="selectEmoji(emoji.icon)"
+          >
             {{ emoji.icon }}
           </div>
         </div>
@@ -25,34 +39,33 @@ export default {
 
   props: {
     inputBox: {
-      require: true
+      require: true,
     },
   },
 
   data() {
     return {
       menu: false,
-      emojis: require('./emoji.json')
-    }
+      emojis: require("./emoji.json"),
+    };
   },
 
   computed: {
     emojiGroup() {
       let groups = [];
 
-      this.emojis.forEach(emoji => {
-        if (groups.indexOf(emoji.group) < 0)
-          groups.push(emoji.group);
+      this.emojis.forEach((emoji) => {
+        if (groups.indexOf(emoji.group) < 0) groups.push(emoji.group);
       });
 
       return groups;
-    }
+    },
   },
 
   methods: {
     selectEmoji(emoji) {
       const selector = this.inputBox;
-      const el = selector.$el.querySelector('textarea')
+      const el = selector.$el.querySelector("textarea");
 
       let currentInput = selector.value;
       let startPosition = el.selectionStart;
@@ -61,18 +74,16 @@ export default {
       let textBefore = currentInput.substring(0, startPosition);
       let textAfter = currentInput.substring(endPosition, currentInput.length);
 
-      selector.$emit('input', textBefore + emoji + textAfter);
-      this.$emit('close')
+      selector.$emit("input", textBefore + emoji + textAfter);
+      this.$emit("close");
 
       this.$nextTick(function () {
         el.focus();
         el.setSelectionRange(startPosition, startPosition + 1);
-      })
-    }
-  }
-}
+      });
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
