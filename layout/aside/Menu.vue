f1d5a2a6f387fee486cb6c866355c545bb973049
RezaParsian <rezaparsian76@gmail.com>
2025-09-25 08:53:41 +0330

    Add loading to payment link.

--- layout/aside/Menu.vue
+++ layout/aside/Menu.vue
@@ -1,36 +1,36 @@
 <template>
   <v-list v-if="rerender" dark>
     <custom-popup
-      v-model="openPaymentUrlLink"
-      :cons="{ title: 'لینک پرداخت' }"
-      hide-overlay
-      hide-confirm
-      max-width="500"
-      transition="dialog-bottom-transition"
+        v-model="openPaymentUrlLink"
+        :cons="{ title: 'لینک پرداخت' }"
+        hide-overlay
+        hide-confirm
+        max-width="500"
+        transition="dialog-bottom-transition"
     >
       <v-card class="p-5">
         <v-text-field
-          dense
-          label="شماره کاربر"
-          outlined
-          class="centered-input"
-          v-model="userPhone"
-          :rules="[phoneRule]"
+            dense
+            label="شماره کاربر"
+            outlined
+            class="centered-input"
+            v-model="userPhone"
+            :rules="[phoneRule]"
         />
 
         <price-input
-          v-model="amount"
-          :label="i18n.t('WALLET.Amount')"
-          text-center="true"
+            v-model="amount"
+            :label="i18n.t('WALLET.Amount')"
+            text-center="true"
         />
 
         <div class="row col justify-content-end mx-auto">
           <v-btn
-            @click="sendLink"
-            color="green"
-            class="mr-auto"
-            :loading="loading"
-            :disabled="!userPhone && amount === 0"
+              @click="sendLink"
+              color="green"
+              class="mr-auto"
+              :loading="loading"
+              :disabled="!userPhone && amount === 0"
           >
             <v-icon class="ml-2">mdi-send</v-icon>
             ارسال
@@ -43,9 +43,9 @@
       <v-list-item-content>
         <div class="text-center">
           <img
-            :src="DConfigs.header_logo"
-            alt="Logo"
-            class="logo-default max-h-40px"
+              :src="DConfigs.header_logo"
+              alt="Logo"
+              class="logo-default max-h-40px"
           />
         </div>
       </v-list-item-content>
@@ -56,7 +56,7 @@
       </v-list-item-action>
     </v-list-item>
 
-    <v-divider style="background-color: rgba(255, 255, 255, 0.3)" />
+    <v-divider style="background-color: rgba(255, 255, 255, 0.3)"/>
 
     <v-list-item :href="homeLink()" link @click="toggleMobileMenu()">
       <v-list-item-icon>
@@ -78,7 +78,7 @@
           <v-icon>mdi-circle-small</v-icon>
         </v-list-item-icon>
         <v-list-item-title>
-          <v-skeleton-loader class="mb-n1" type="text" />
+          <v-skeleton-loader class="mb-n1" type="text"/>
         </v-list-item-title>
       </v-list-item>
     </div>
@@ -98,16 +98,16 @@
       <template v-for="(item, i) in items">
         <div v-if="!!item" :key="`item-${i}`" class="group-wrapper">
           <v-list-item
-            v-if="!item.children"
-            :to="`/${item.slug}`"
-            link
-            @click="toggleMobileMenu()"
+              v-if="!item.children"
+              :to="`/${item.slug}`"
+              link
+              @click="toggleMobileMenu()"
           >
             <v-list-item-icon>
               <v-icon>mdi-circle-small</v-icon>
             </v-list-item-icon>
             <v-list-item-title
-              v-text="
+                v-text="
                 'MENUS.' + item.name !== i18n.t('MENUS.' + item.name)
                   ? i18n.t('MENUS.' + item.name)
                   : item.name
@@ -116,38 +116,38 @@
           </v-list-item>
 
           <v-list-group
-            v-else
-            v-model="item.selectedItem"
-            :value="getActive(item)"
-            active-class="active-child"
-            color="white"
-            no-action
-            @click="item.selectedItem = true"
+              v-else
+              v-model="item.selectedItem"
+              :value="getActive(item)"
+              active-class="active-child"
+              color="white"
+              no-action
+              @click="item.selectedItem = true"
           >
             <template v-slot:activator>
               <v-list-item-title
-                >{{
+              >{{
                   "MENUS." + item.name !== i18n.t("MENUS." + item.name)
-                    ? i18n.t("MENUS." + item.name)
-                    : item.name
+                      ? i18n.t("MENUS." + item.name)
+                      : item.name
                 }}
               </v-list-item-title>
             </template>
 
             <v-list-item
-              v-for="(sub, j) in item.children"
-              :key="j"
-              :to="`/${sub.slug}`"
-              color="white"
-              link
-              style="padding-right: 32px"
-              @click="toggleMobileMenu()"
+                v-for="(sub, j) in item.children"
+                :key="j"
+                :to="`/${sub.slug}`"
+                color="white"
+                link
+                style="padding-right: 32px"
+                @click="toggleMobileMenu()"
             >
               <v-list-item-icon>
                 <v-icon>mdi-menu-left</v-icon>
               </v-list-item-icon>
               <v-list-item-title
-                v-text="
+                  v-text="
                   i18n.t('MENUS.' + sub.name) !== 'MENUS.' + sub.name
                     ? i18n.t('MENUS.' + sub.name)
                     : sub.name
@@ -167,9 +167,9 @@
     </v-list-item>
 
     <v-list-item
-      link
-      @click="openPaymentUrlLink = true"
-      v-if="ribbon_can('send_payment_link')"
+        link
+        @click="openPaymentUrlLink = true"
+        v-if="ribbon_can('send_payment_link')"
     >
       <v-list-item-icon>
         <v-icon color="#ff5b94">mdi-hand-coin</v-icon>
@@ -180,15 +180,15 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-import { reformatMenuResponse } from "../../assets/js/MenuFunctions";
+import {mapActions, mapGetters} from "vuex";
+import {reformatMenuResponse} from "../../assets/js/MenuFunctions";
 import i18n from "../../plugins/EasyModal/i18n";
 import CustomPopup from "../../plugins/popup/customPopup.vue";
 import priceInput from "../../pages/pickers/priceInput.vue";
 
 export default {
   name: "AsideMenu",
-  components: { priceInput, CustomPopup },
+  components: {priceInput, CustomPopup},
   props: {
     visible: Boolean,
   },
@@ -227,30 +227,30 @@ export default {
   methods: {
     ...mapActions("ribbon", ["setMenus", "toggleLoading", "setSectionStatus"]),
     getMenus() {
-      this.toggleLoading({ field: "menus", status: true });
+      this.toggleLoading({field: "menus", status: true});
       try {
         this.$DashboardAxios
-          .get("/api/core/menus", {
-            params: {
-              sid: this.sid,
-            },
-          })
-          .then(({ data }) => {
-            const menus = reformatMenuResponse(data.data);
-            console.log("menus", menus);
-            this.setMenus(menus);
-            this.setSectionStatus({ field: "menus", status: true });
-            this.items = menus;
-          })
-          .catch(({ response }) => {
-            this.setSectionStatus({ field: "menus", status: false });
-            if (response.status !== 401) {
-              this.$toast.error("خطایی رخ داده است.");
-            }
-          })
-          .finally(() => this.toggleLoading({ field: "menus", status: false }));
+            .get("/api/core/menus", {
+              params: {
+                sid: this.sid,
+              },
+            })
+            .then(({data}) => {
+              const menus = reformatMenuResponse(data.data);
+              console.log("menus", menus);
+              this.setMenus(menus);
+              this.setSectionStatus({field: "menus", status: true});
+              this.items = menus;
+            })
+            .catch(({response}) => {
+              this.setSectionStatus({field: "menus", status: false});
+              if (response.status !== 401) {
+                this.$toast.error("خطایی رخ داده است.");
+              }
+            })
+            .finally(() => this.toggleLoading({field: "menus", status: false}));
       } catch (e) {
-        this.setSectionStatus({ field: "menus", status: false });
+        this.setSectionStatus({field: "menus", status: false});
         console.log("catch error: ", e);
       }
     },
@@ -259,8 +259,8 @@ export default {
     },
     getActive(item) {
       let activate = item.children
-        ?.map((a) => a.slug)
-        .includes(this.$route.path.substring(1));
+          ?.map((a) => a.slug)
+          .includes(this.$route.path.substring(1));
       return (item.selectedItem = activate);
     },
     downloadDorsandesk() {
@@ -277,24 +277,27 @@ export default {
       }, 2000);
     },
     sendLink() {
-      this.downloading = true;
+      this.loading = true;
 
       this.$instanceAxios
-        .post("https://robot-api.terminalads.com/api/user/bazar/payment/link", {
-          amount: this.amount,
-          phone: this.userPhone,
-        })
-        .then(() => {
-          this.$toast.info("لینک پرداخت با موفقیت ارسال شد!");
-        })
-        .catch(({ response }) => {
-          if (response.status === 404) {
-            this.$toast.error("کاربر موردنظر پیدا نشد!");
-          }
-        })
-        .finally(() => {
-          this.loading = false;
-        });
+          .post("https://robot-api.terminalads.com/api/user/bazar/payment/link", {
+            amount: this.amount,
+            phone: this.userPhone,
+          })
+          .then(() => {
+            this.$toast.info("لینک پرداخت با موفقیت ارسال شد!");
+            this.openPaymentUrlLink = false;
+            this.userPhone = undefined;
+            this.amount = 0;
+          })
+          .catch(({response}) => {
+            if (response.status === 404) {
+              this.$toast.error("کاربر موردنظر پیدا نشد!");
+            }
+          })
+          .finally(() => {
+            this.loading = false;
+          });
     },
   },
 };
