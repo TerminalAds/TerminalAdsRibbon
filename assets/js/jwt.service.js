1a6d9bc239c6b74cc6809f3f9735e23535c0d59d
RezaParsian <rezaparsian76@gmail.com>
2025-10-09 11:31:12 +0330

    add search param to destroyToken

--- assets/js/jwt.service.js
+++ assets/js/jwt.service.js
@@ -1,23 +1,30 @@
+import {setLanguageCookie} from "../../utils/languageCookie";
+
 const ID_TOKEN_KEY = "id_token";
 
 export const getToken = () => {
-  return window.localStorage.getItem(ID_TOKEN_KEY);
+	return window.localStorage.getItem(ID_TOKEN_KEY);
 };
 
 export const saveToken = token => {
-  window.localStorage.setItem(ID_TOKEN_KEY, token);
+	window.localStorage.setItem(ID_TOKEN_KEY, token);
 };
 
 export const destroyToken = (origin, hash) => {
-  window.localStorage.removeItem(ID_TOKEN_KEY);
-  localStorage.removeItem('reloaded');
-  window.localStorage.removeItem('userInfo');
+	const url = new URL(location.href);
+	window.localStorage.removeItem(ID_TOKEN_KEY);
+	localStorage.removeItem('reloaded');
+	window.localStorage.removeItem('userInfo');
 
-  (!!origin && !!hash)
-      ? window.location.replace(`https://core.terminalads.com/#/login?call_back_origin=${origin}&call_back_hash=${hash}`)
-      : window.location.origin === 'https://core.terminalads.com'
-          ? window.location.reload()
-          : window.location.replace(`https://core.terminalads.com/#/login`)
+	if (url.searchParams.has('lang')) {
+		setLanguageCookie(url.searchParams.get('lang'));
+	}
+
+	(!!origin && !!hash)
+		? window.location.replace(`https://core.terminalads.com/#/login?call_back_origin=${origin}&call_back_hash=${hash}`)
+		: window.location.origin === 'https://core.terminalads.com'
+			? window.location.reload()
+			: window.location.replace(`https://core.terminalads.com/#/login`);
 };
 
 export default {getToken, saveToken, destroyToken};
