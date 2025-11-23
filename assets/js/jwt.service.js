f2a94752894e64ce350820e5c8bd5db96974391d
homa <5700terminalads@gmail.com>
2025-11-23 22:22:26 +0330

    fix bug

--- assets/js/jwt.service.js
+++ assets/js/jwt.service.js
@@ -23,14 +23,13 @@ export const destroyToken = (origin, hash) => {
   const loginUrl = Vue.prototype.$coreLogin;
   !!origin && !!hash
     ? window.location.replace(
-        window.location.replace(
-          `${loginUrl}?call_back_origin=${origin}&call_back_hash=${hash}`
-        )
+        `${loginUrl}?call_back_origin=${origin}&call_back_hash=${hash}`
       )
     : window.location.origin === "https://core.terminalads.com" ||
       window.location.origin === "https://core.terminalads.ir"
     ? window.location.reload()
     : window.location.replace(loginUrl);
+
 };
 
 export default { getToken, saveToken, destroyToken };
